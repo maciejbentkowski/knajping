ae8a181ff303aeb2f43076a3877ee6d9c99c2a12
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-02-12 16:34:47 +0100

    added validations and coverage to git ignore

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+  validates :username, presence: true
 
   has_many :venues, dependent: :nullify
 
--- spec/controllers/venues_controller_spec.rb
+++ spec/controllers/venues_controller_spec.rb
@@ -1,10 +1,17 @@
 require "rails_helper"
 
 RSpec.describe VenuesController, type: :controller do
-    describe "GET index" do
+    describe "GET #index" do
         it "has a 200 status code" do
           get :index
           expect(response.status).to eq(200)
         end
+    end
+
+    describe "GET #show" do
+      it "has a 200 status code" do
+        get :show
+        expect(response.status).to eq(200)
+      end
     end
 end
