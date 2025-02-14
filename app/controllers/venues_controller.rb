1ddcd2ae55a7e31994890967c725eb87142cdf39
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-02-14 15:56:41 +0100

    Added Venue#show page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,14 +1,22 @@
 class VenuesController < ApplicationController
+    before_action :set_venue, only: [:show]
     authorize_resource
 
     def index
         @venues = Venue.all
     end
 
+    def show
+
+    end
+
 
     private
-
     def current_ability
         @current_ability ||= VenueAbility.new(current_user)
+    end
+
+    def set_venue
+        @venue = Venue.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root to: "pages#main"
 
-  resources :venues, only: [ :index ]
+  resources :venues, only: [ :index, :show ]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
