772b720af8d79dc83c03d2f57830edd15e1dede7
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-02-14 15:07:26 +0100

    Adapt venue#index page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,11 +2,11 @@ class VenuesController < ApplicationController
     authorize_resource
 
     def index
+        @venues = Venue.all
     end
 
 
     private
-
 
     def current_ability
         @current_ability ||= VenueAbility.new(current_user)
