a2131a1d381fd05ac73cce7f255d4e7c906d7c7b
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-02-26 21:46:49 +0100

    Added validation to VenueType

--- app/models/venue_type.rb
+++ app/models/venue_type.rb
@@ -1,4 +1,6 @@
 class VenueType < ApplicationRecord
     has_many :venue_venue_types, dependent: :destroy
     has_many :venues, through: :venue_venue_types
+
+    validates :name, presence: true, uniqueness: true
 end
