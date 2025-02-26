19445c0e019ed35ed34ec90199450a61848bf149
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-02-26 21:47:44 +0100

    Added validations to VenueVenueType model

--- app/models/venue_venue_type.rb
+++ app/models/venue_venue_type.rb
@@ -1,4 +1,7 @@
 class VenueVenueType < ApplicationRecord
   belongs_to :venue
   belongs_to :venue_type
+
+  validates :importance, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 3 }
+  validates :venue_id, uniqueness: { scope: :venue_type_id, message: "already has this type assigned" }
 end
