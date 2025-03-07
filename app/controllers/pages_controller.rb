4718387913be29e8710ec485cd89f1b9110c6160
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-03-07 14:28:08 +0100

    Included avatar to Review in Main page recent reviews section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
     def main
         @featured_venues = Venue.featured_venues
-        @recent_reviews = Review.recent.includes(:venue, :user, :rating)
+        @recent_reviews = Review.recent.includes(:venue, :user, :rating, user: :avatar_attachment)
     end
 end
