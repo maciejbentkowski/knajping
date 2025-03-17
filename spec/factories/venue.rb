a0a84e731d26adc6acc007fdbde0d3102335a611
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-03-17 21:28:08 +0100

    Added avg rating update for seeds

--- spec/factories/venue.rb
+++ spec/factories/venue.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
       sequence(:name) { |n| "Venue #{n}" }
       is_activate { true }
       association :user
+      sequence(:address) { |n| "Sample address #{n}" }
+      postal_code { "01-910" }
+      city { "Warsaw" }
+      latitude { rand(52.18..52.31) }
+      longitude { rand(20.87..21.08) }
     end
   end
