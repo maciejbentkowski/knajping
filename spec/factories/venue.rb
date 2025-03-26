6037d0dbe4a5fb7f99fec4c34d8616d7fe84a8e6
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-03-26 20:54:47 +0100

    Added Stub for geocoding feature for venues

--- spec/factories/venue.rb
+++ spec/factories/venue.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
-    factory :venue do
-      sequence(:name) { |n| "Venue #{n}" }
-      is_activate { true }
-      association :user
-      sequence(:address) { |n| "Sample address #{n}" }
-      postal_code { "01-910" }
-      city { "Warsaw" }
-      latitude { rand(52.18..52.31) }
-      longitude { rand(20.87..21.08) }
-    end
+  factory :venue do
+    sequence(:name) { |n| "Venue #{n}" }
+    is_activate { true }
+    association :user
+    sequence(:address) { |n| "Sample address #{n}" }
+    postal_code { "01-910" }
+    city { "Warsaw" }
+    latitude { rand(52.18..52.31) }
+    longitude { rand(20.87..21.08) }
   end
+   
+end
---
+++ spec/support/geocoder.rb
@@ -0,0 +1,12 @@
+Geocoder::Lookup::Test.set_default_stub(
+    [
+      {
+        'coordinates'  => [52.2297, 21.0122],
+        'address'      => 'Warsaw, Poland',
+        'city'         => 'Warsaw',
+        'state'        => 'Mazovia',
+        'country'      => 'Poland',
+        'country_code' => 'PL'
+      }
+    ]
+  )
