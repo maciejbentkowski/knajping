8e4a48a78e9991a23e83d1bd718275927f9bd4a0
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-03-28 16:39:01 +0100

    Added venues requests spec for Index, Show and New actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden }
-      format.html { redirect_to root_path, alert: exception.message }
+    if exception.action == :new && current_user == nil
+      respond_to do |format|
+        format.json { head :forbidden }
+        format.html { redirect_to new_user_session_path, alert: exception.message }
+      end
+    else
+      respond_to do |format|
+        format.json { head :forbidden }
+        format.html { redirect_to root_path, alert: exception.message }
+      end
     end
   end
 end
--- spec/requests/venues_spec.rb
+++ spec/requests/venues_spec.rb
@@ -1,4 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "Venues", type: :request do
+    describe "GET /venues" do
+        it "Return 200" do
+            get venues_path
+            expect(response).to have_http_status(200)
+        end
+    end
+    describe "GET /venues/:id" do
+        let(:venue) { create(:venue) }
+
+        it "Return 200" do
+            get venue_path(venue)
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    describe "GET /venues/new" do
+        context "User not logged in" do
+            it "Return 302" do
+                get new_venue_path
+                expect(response).to have_http_status(302)
+            end
+
+            it "Redirect to login_path" do
+                get new_venue_path
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+
+        context "Owner user logged in" do
+            let(:user) { create(:owner) }
+            it "Return 200" do
+                sign_in(user)
+                get new_venue_path
+                expect(response).to have_http_status(200)
+            end
+        end
+        context "Reviewer user logged in" do
+            let(:user) { create(:user) }
+
+            it "Return 302" do
+                sign_in(user)
+                get new_venue_path
+                expect(response).to have_http_status(302)
+            end
+
+            it "Redirect to root path" do
+                get new_venue_path
+                sign_in(user)
+                get new_venue_path
+                expect(response).to redirect_to(root_path)
+            end
+        end
+    end
 end
