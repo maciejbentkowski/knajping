6669f8718f2b4cbd069bad65ad7388a99bafc5b1
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-04-04 16:10:55 +0200

    Created questions controller and abilities

---
+++ app/abilities/question_ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class QuestionAbility
+    include CanCan::Ability
+
+    def initialize(user)
+      can :read, Question
+
+      return unless user.present?
+
+      if user.reviewer?
+        can [ :new, :create ], Question
+        can [ :edit, :update ], Question do |question|
+          question.user_id == user.id
+        end
+      end
+
+      if user.admin? || user.moderator?
+        can :manage, Question
+      end
+    end
+end
