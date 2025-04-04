33cad37ce3b0ef938360d673076ab0c579bc3ec2
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-04-04 13:07:15 +0200

    Added sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+Sentry.init do |config|
+  config.environment = 'production'
+  config.dsn = 'https://78d47b91fe81620315ed7e7b55597421@o4509073159028736.ingest.de.sentry.io/4509073160274000'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  config.send_default_pii = true
+end
