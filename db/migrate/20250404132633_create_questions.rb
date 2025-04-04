2a031288c7ac42bab0746c12683c1f31affd22f9
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-04-04 15:47:19 +0200

    Added question migration

--- db/migrate/20250404132633_create_questions.rb
+++ db/migrate/20250404132633_create_questions.rb
@@ -1,6 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[8.0]
   def change
     create_table :questions do |t|
+      t.text :body
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_21_104013) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_04_132633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_21_104013) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "ratings", force: :cascade do |t|
@@ -116,6 +124,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_21_104013) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "questions", "users"
   add_foreign_key "ratings", "reviews"
   add_foreign_key "reviews", "users"
   add_foreign_key "reviews", "venues"
