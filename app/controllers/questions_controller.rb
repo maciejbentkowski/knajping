c79e41c33ee9d04cb6e0e9a24722cce16d41352a
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-04-08 21:04:56 +0200

    Added question destroy method and test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,9 @@ class QuestionsController < ApplicationController
     def edit
         @question = Question.find(params[:id])
     end
+
+    def destroy
+        @question = Question.find(params[:id])
+        @question.destroy!
+    end
 end
--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Questions", type: :request do
+
+    describe "DELETE /questions/:id" do
+        context "when user owns the question" do
+        let(:user) { create(:user) }
+        let(:question) { create(:question, user: user) }
+        it "removes a question" do
+            sign_in(user)
+            question
+            expect do
+            delete question_path(question)
+            end.to change(Question, :count).by(-1)
+        end
+    end
 end
+end
+
+
