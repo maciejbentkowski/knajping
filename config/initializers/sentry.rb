e65a8b274b8fdfa8a8a64efe2d089952d23af966
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-04-10 12:55:23 +0200

    Added another config value to sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,6 +5,6 @@ Sentry.init do |config|
   config.enabled_environments = %w[production]
   config.dsn = "https://78d47b91fe81620315ed7e7b55597421@o4509073159028736.ingest.de.sentry.io/4509073160274000"
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-
+  config.traces_sample_rate = 1.0
   config.send_default_pii = true
 end
