cee31355a3177319afd22671462a49683c7e89a1
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2025-05-21 20:27:35 +0200

    Added Users to admin panel and deleted all unnecessary files

--- app/controllers/admin/panel_controller.rb
+++ app/controllers/admin/panel_controller.rb
@@ -1,9 +1,11 @@
 class Admin::PanelController < AdminController
     def index
-        @menu_items = { "Venues" => "venues", "Venue Types" => "venue-types" } # changed : to => for proper string keys
+        @menu_items = { "Users" => "users", "Venues" => "venues", "Venue Types" => "venue-types" } # changed : to => for proper string keys
         @value = params[:value]
         @collection =
           case @value
+          when "users"
+            User.all
           when "venues"
             Venue.order(:name).includes(:user)
           when "venue-types"
